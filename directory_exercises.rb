3deb14a17a78ef7017d9d87a2b4ae1dbc4f6355e
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-10-02 13:01:11 +0100

    added ability to save student list to file, added gitignore

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -26,6 +26,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -36,6 +38,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -110,6 +113,16 @@ end
 def print_footer
   student_count = @students.count
   puts "Overall, we have #{student_count} great #{student_count == 1 ? "student" : "students"}".center(50)
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts(csv_line)
+  end
+  file.close
 end
 
 interactive_menu
